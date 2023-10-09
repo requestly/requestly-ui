bd4508950434d6a51391808cfc2cd1f9f6c02e13
RuntimeTerror10 <parthb.git@gmail.com>
2023-10-09 09:39:12 +0530

    support for enable autoscroll and row styles

--- packages/resource-table/src/ResourceTableRow.tsx
+++ packages/resource-table/src/ResourceTableRow.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement, memo, useMemo } from "react";
+import React, { CSSProperties, ReactElement, memo, useMemo } from "react";
 import { Table } from "@devtools-ds/table";
 import { Column } from "./types";
 
@@ -8,7 +8,7 @@ interface Props<ResourceType> {
   columns: Column<ResourceType>[];
   isFailed?: (resource: ResourceType) => boolean;
   setContextMenuSelectedResource: (resource: ResourceType) => void;
-  rowClassName?: (log: ResourceType) => string | string;
+  rowStyle?: (log: ResourceType) => CSSProperties | CSSProperties;
 }
 
 const ResourceTableRow = <ResourceType,>({
@@ -17,7 +17,7 @@ const ResourceTableRow = <ResourceType,>({
   columns,
   isFailed,
   setContextMenuSelectedResource,
-  rowClassName,
+  rowStyle,
 }: Props<ResourceType>): ReactElement => {
   const failed = useMemo(() => isFailed?.(resource), [resource]);
 
@@ -25,9 +25,8 @@ const ResourceTableRow = <ResourceType,>({
     <Table.Row
       id={id}
       onContextMenu={() => setContextMenuSelectedResource(resource)}
-      className={`rq-resource-table-row ${failed ? "failed" : ""} ${
-        typeof rowClassName === "string" ? rowClassName : rowClassName?.(resource)
-      }`}
+      className={`rq-resource-table-row ${failed ? "failed" : ""}`}
+      style={typeof rowStyle === "object" ? rowStyle : rowStyle?.(resource)}
     >
       {columns.map((column) => (
         <Table.Cell key={column.key}>{column.render(resource)}</Table.Cell>
